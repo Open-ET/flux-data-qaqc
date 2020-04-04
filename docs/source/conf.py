17208430249cd808e6c97bd0ba9679678447d485
John Volk <johnvolk08@gmail.com>
2020-04-04 15:32:26 -0400

    update autodoc mocking libraries for RTD API build

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,7 +16,9 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('../../'))
 
-autodoc_mock_imports = ["numpy", "bokeh", "pandas", "xarray", "refet"]
+autodoc_mock_imports = [
+    "numpy", "bokeh", "pandas", "xarray", "refet", "sklearn", "netCDF4"
+]
 
 
 # -- Project information -----------------------------------------------------
